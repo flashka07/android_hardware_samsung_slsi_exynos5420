c7e034952661373b69470847cbbe09de1f16d789
Ivan Drozdov <idrozdov@smartlabs.tv>
2014-09-18 14:46:46 +0400

    IMPR: do not use local libcurl (use from external dir)

--- mobicore/Android.mk
+++ mobicore/Android.mk
@@ -34,8 +34,7 @@ GDM_PROVLIB_SHARED_LIBS=libMcClient
 include $(MOBICORE_PROJECT_PATH)/provlib/Android.mk
 
 
-MOBICORE_DIR_INC := $(MC_INCLUDE_DIR) $(MOBICORE_PROJECT_PATH)/common/curl/include
-include $(MOBICORE_PROJECT_PATH)/common/curl/Android.mk
+MOBICORE_DIR_INC := $(MC_INCLUDE_DIR)
 include $(MOBICORE_PROJECT_PATH)/rootpa/Code/Android/app/jni/Android.mk
 include $(MOBICORE_PROJECT_PATH)/rootpa/Code/Android/lib/Android.mk
 include $(MOBICORE_PROJECT_PATH)/rootpa/Code/Android/app/Android.mk
